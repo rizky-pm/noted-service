fc3e04cc611dc8ec0ea841e2da9e381a6d1d22af
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2025-04-21 10:18:56 +0700

    fix: update CORS configuration to use different ports for production and development

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,8 +1,8 @@
 const corsConfig = {
   origin:
     process.env.NODE_ENV === 'production'
-      ? 'http://103.63.25.22:3000'
-      : 'http://localhost:3000',
+      ? 'http://103.63.25.22:5173'
+      : 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 };
