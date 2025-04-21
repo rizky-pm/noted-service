89ab9270856c8ea51e269398d553da4a62907109
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2025-04-21 10:03:59 +0700

    fix: update CORS configuration to use different origins based on environment

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,5 +1,8 @@
 const corsConfig = {
-  origin: 'http://localhost:3000',
+  origin:
+    process.env.NODE_ENV === 'production'
+      ? 'http://103.63.25.22:3000'
+      : 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 };
